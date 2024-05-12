070a643f01232dd7d9e060c3a6ea67306869a027
JooSung02 <89979771+JooSung02@users.noreply.github.com>
2024-05-12 21:35:28 +0900

    그래프-연결리스트

--- 그래프1.c
+++ 그래프1.c
@@ -1,56 +1,66 @@
 #define MAX_VERTICES 50
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
+
+typedef struct GraphNode
+{
+	int vertex;
+	struct GraphNode* link;
+}GraphNode;
 
 typedef struct GraphType {
 	int n;  // 정점의 개수
-	int adj_mat[MAX_VERTICES][MAX_VERTICES];
+	GraphNode* adj_list[MAX_VERTICES];
 }GraphType;
 
 // 그래프 초기화
-void init(GraphType* g)
+void init(GraphType* g)  
 {
-	int r, c;
+	int v;
 	g->n = 0;
-	for (r = 0; r < MAX_VERTICES; r++)
-		for (c = 0; c < MAX_VERTICES; c++)
-			g->adj_mat[r][c] = 0;
+	for (v = 0; v < MAX_VERTICES; v++)
+		g->adj_list[v] = NULL;
 }
 
 // 정점 삽입 연산
 void insert_vertex(GraphType* g, int v)
 {
 	if (((g->n) + 1) > MAX_VERTICES) {
-		printf("그래프: 정점의 개수 초과");
+		printf("그래프: 점점의 개수 초과");
 		return;
 	}
 	g->n++;
 }
 
-// 간선 삽입 연산
-void insert_edge(GraphType* g, int start, int end)
+// 간선 삽입 연산, v를u의 인접 리스트에 삽입한다.
+void insert_edge(GraphType* g, int u, int v)
 {
-	if (start >= g->n || end >= g->n) {
+	GraphNode* node;
+	if (u >= g->n || v >= g->n)
+	{
 		printf("그래프: 정점 번호 오류");
 		return;
 	}
-	g->adj_mat[start][end] = 1;
-	g->adj_mat[end][start] = 1;
+	node = (GraphNode*)malloc(sizeof(GraphNode));
+	node->vertex = v;
+	node->link = g->adj_list[u];
+	g->adj_list[u] = node;
 }
 
-// 인접 행렬 출력 함수
-void print_adj_mat(GraphType* g)
+void print_adj_list(GraphType* g)
 {
 	for (int i = 0; i < g->n; i++) {
-		for (int j = 0; j < g->n; j++) {
-			printf("%2d", g->adj_mat[i][j]);
+		GraphNode* p = g->adj_list[i];
+		printf("정점 %d의 인접 리스트",i);
+		while (p != NULL) {
+			printf("-> %d", p->vertex);
+			p = p->link;
 		}
 		printf("\n");
 	}
 }
 
-void main()
+int main()
 {
 	GraphType* g;
 	g = (GraphType*)malloc(sizeof(GraphType));
@@ -58,11 +68,16 @@ void main()
 	for (int i = 0; i < 4; i++)
 		insert_vertex(g, i);
 	insert_edge(g, 0, 1);
+	insert_edge(g, 1, 0);
 	insert_edge(g, 0, 2);
+	insert_edge(g, 2, 0);
 	insert_edge(g, 0, 3);
+	insert_edge(g, 3, 0);
 	insert_edge(g, 1, 2);
+	insert_edge(g, 2, 1);
 	insert_edge(g, 2, 3);
-	print_adj_mat(g);
-
+	insert_edge(g, 3, 2);
+	print_adj_list(g);
 	free(g);
+	return 0;
 }
